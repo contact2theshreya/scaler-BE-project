4c0921859d07371fa29a032830a3ba303d31773a
Shreya Gupta <contact2theshreya@gmail.com>
2024-10-21 16:51:42 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication implements CommandLineRunner {
-
+//The Spring Boot CLI (Command Line Interface) is present in the dependency of the org.springframework.boot that is used to quickly bootstrap the spring boot application. CLI in Spring Boot contains a run method() that is executed after the application startup.
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
